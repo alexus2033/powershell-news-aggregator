777494d457b1cc79ccf6f2f33bb3031a11dc37f6
alexus2033 <85680663+alexus2033@users.noreply.github.com>
2022-08-28 16:28:28 +0200

    handle spanish times in invidious

--- commonVideo.ps1
+++ commonVideo.ps1
@@ -23,28 +23,47 @@ function FormatTime($timestamp){
 }
 
 function global:CalcTime([string]$info){
-   $part = $info -split '\s+'
-   if($part[1] -match '^[0-9]+$'){
-      $number = [int]$part[1]
-      if($part[2].StartsWith("minute")){
-         return (get-date).AddMinutes($number*-1)
-      }
-      if($part[2].StartsWith("hour")){
-         return (get-date).AddHours($number*-1)
-      }
-      if($part[2].StartsWith("day")){
-         return (get-date).AddDays($number*-1)
-      }
-      if($part[2].StartsWith("week")){
-         return (get-date).AddDays($number*-7)
-      }
-      if($part[2].StartsWith("month")){
-       return (get-date).AddDays($number*-30)
-      }
-   }
- }
+    $part = $info -split '\s+'
+    if($part[1] -match '^[0-9]+$'){
+        $number = [int]$part[1]
+        if($part[2].StartsWith("minute")){
+            return (get-date).AddMinutes($number*-1)
+        }
+        if($part[2].StartsWith("hour")){
+            return (get-date).AddHours($number*-1)
+        }
+        if($part[2].StartsWith("day")){
+            return (get-date).AddDays($number*-1)
+        }
+        if($part[2].StartsWith("week")){
+            return (get-date).AddDays($number*-7)
+        }
+        if($part[2].StartsWith("month")){
+        return (get-date).AddDays($number*-30)
+        }
+    }
+    if($part[2] -match '^[0-9]+$'){
+        $number = [int]$part[2]
+        if($part[3].StartsWith("minuto")){
+            return (get-date).AddMinutes($number*-1)
+        }
+        if($part[3].StartsWith("horas")){
+            return (get-date).AddHours($number*-1)
+        }
+        if($part[3].StartsWith("semana")){
+            return (get-date).AddDays($number*-7)
+        }
+        if($part[3].StartsWith("d")){  #compartido hace 2 días
+            return (get-date).AddDays($number*-1)
+        }
+        if($part[3].StartsWith("mes")){
+            return (get-date).AddDays($number*-30)
+        }
+    }
+    return $info
+}
  
-function ReadVideoUrl($index = 1){
+function global:ReadVideoUrl($index = 1){
    # Ensures that Invoke-WebRequest uses TLS 1.2
    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
    $videoSearch = "https://api.invidious.io/instances.json?pretty=1&sort_by=type,health"
@@ -59,29 +78,7 @@ function global:ReadInvidious([string[]] $channels, [int] $maxChanItems = 1){
       param( [String]$chan,
              [string]$videoSearch,
              $maxChanItems = 1 )
-
-             function CalcTime([string]$info){
-               $part = $info -split '\s+'
-               if($part[1] -match '^[0-9]+$'){
-                  $number = [int]$part[1]
-                  if($part[2].StartsWith("minute")){
-                     return (get-date).AddMinutes($number*-1)
-                  }
-                  if($part[2].StartsWith("hour")){
-                     return (get-date).AddHours($number*-1)
-                  }
-                  if($part[2].StartsWith("day")){
-                     return (get-date).AddDays($number*-1)
-                  }
-                  if($part[2].StartsWith("week")){
-                     return (get-date).AddDays($number*-7)
-                  }
-                  if($part[2].StartsWith("month")){
-                   return (get-date).AddDays($number*-30)
-                  }
-               }
-             }
-            
+   
       $agents = [Microsoft.PowerShell.Commands.PSUserAgent]
       $x=(Get-random -Minimum 1 -Maximum $agents.GetProperties().count)
       $agent = $agents.GetProperties()[$x-1]
@@ -90,7 +87,7 @@ function global:ReadInvidious([string[]] $channels, [int] $maxChanItems = 1){
       [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
       $newsList = [System.Collections.ArrayList]@()
 
-      $webResult = Invoke-WebRequest "$videoSearch/channel/$chan" -UserAgent $agent #-Headers $global:headers
+      $webResult = Invoke-WebRequest "$videoSearch/channel/$chan" -UserAgent GetAgent #-Headers global:headers
       $htmldom = ConvertFrom-Html $webResult
   
       $chanTitle = $htmldom.SelectNodes("//div/div[@class='channel-profile']/span")
@@ -109,7 +106,7 @@ function global:ReadInvidious([string[]] $channels, [int] $maxChanItems = 1){
             Channel = $chanTitle.innerhtml
             Title = $title.innerhtml
             Link  = "<a href='$link' target='_blank'>$($chanTitle.innerhtml)</a>"
-            Date =  (CalcTime $timeText)
+            Date = (CalcTime $timeText)
           }
           # put one row together...                                  
           $newsList.Add($row) > $null;
@@ -117,39 +114,7 @@ function global:ReadInvidious([string[]] $channels, [int] $maxChanItems = 1){
       return $newsList
    }
    
-   $videoSearch = ReadVideoUrl
+   $videoSearch = global:ReadVideoUrl
    $resultList = global:Worker $readVideos $channels $videoSearch
    return $resultList  
-}
-
-function global:ReadInvidious2([string[]] $channels){
-
-   $videoSearch = global:ReadVideoUrl
-   $ReportData = [System.Collections.ArrayList]@()
-
-   foreach($chan in $channels){
-
-      $webResult = Invoke-WebRequest "$videoSearch/channel/$chan" -UserAgent $global:agent -Headers $global:headers
-      $htmldom = ConvertFrom-Html $webResult
-
-      $chanTitle = $htmldom.SelectNodes("//div/div[@class='channel-profile']/span")
-
-      $title = $htmldom.SelectNodes("/html/body/div/div[2]/div[9]/div[1]/div/a/p")
-
-      $link = $htmldom.SelectNodes("/html/body/div/div[2]/div[9]/div[1]/div/a")
-      $link = $videoSearch + $link.getattributevalue("href","")
-
-      $timestamp = $htmldom.SelectNodes("//div[1]/div/div[@class='video-card-row flexible']/div[@class='flex-left']/p[@class='video-data']")
-      $timeText = [System.Web.HttpUtility]::HtmlDecode($timestamp.innerText)
-
-      $row = New-Object PSObject -Property @{
-      Channel = $chanTitle.innerhtml
-      Title = $title.innerhtml
-      Link  = "<a href='$link' target='_blank'>$($chanTitle.innerhtml)</a>"
-      Date =  (global:CalcTime $timeText)
-      }
-      # put one row together...                                  
-      $ReportData.Add($row) > $null;
-   }
-   return $ReportData
 }
